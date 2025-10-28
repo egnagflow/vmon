c5d5610c4e2b5ae24b38f58e181d06c63ff7f685
Wolfgang Reißnegger <w.reissnegger@gmx.net>
2025-10-28 11:11:26 -1000

    Add missing copyright header in VIC20 cartridge .mk overlay.

--- targets/vic20/crt_a000.mk
+++ targets/vic20/crt_a000.mk
@@ -1,2 +1,11 @@
+############################################################################
+#
+# (c) 1986-2025 Wolfgang G. Reißnegger <w.reissnegger@gmx.net>
+# 
+# Project: https://github.com/egnagflow/vmon
+# License: https://github.com/egnagflow/vmon/blob/main/LICENSE
+#
+############################################################################
+
 CARTRIDGE_TYPE    = vic20
 CARTRIDGE_ADDRESS = 40960
