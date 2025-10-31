5a70873ccc3141cfde390c97125b41e61cd8415e
Wolfgang Reißnegger <w.reissnegger@gmx.net>
2025-10-31 09:33:01 -1000

    Use vector macros in monitor relocation code.

--- src/mon_relocate.s
+++ src/mon_relocate.s
@@ -11,93 +11,41 @@
 
 .if CONFIG_INIT_RELOCATE_MONITOR
 
-;-----------------------------------------------------------------------------
-vec_src = $10
-vec_dst = $12
+.include "macros.h"
 
 ;-----------------------------------------------------------------------------
 ; Size of the monitor code.
-monitor_len       = (__ROM_LOAD__ + __ROM_SIZE__ - __INIT_LOAD__)
+monitor_len       = (__CODE_END_LOAD__ - __INIT_LOAD__)
 
-; Monitor target start and end address.
+; Monitor target start and end _run_ addresses.
 monitor_dst_start = (__INIT_LOAD__)
-monitor_dst_end   = (__INIT_LOAD__ + monitor_len)
+monitor_dst_end   = (monitor_dst_start + monitor_len)
 
-; Monitor source start and end address.
+; Monitor source start and end _load_ address.
 monitor_src_start = (__BASIC_HEADER_LOAD__ + __BASIC_HEADER_SIZE__)
 monitor_src_end   = (monitor_src_start + monitor_len)
 
 ;-----------------------------------------------------------------------------
 .segment "BASIC_HEADER"
 mon_relocate:
-        ; Disable interrupts while we copy the code around.
-        sei
-
-        ; Save the zeropage pointers we are going to use
-        lda vec_src
-        pha
-        lda vec_src+1
-        pha
-        lda vec_dst
-        pha
-        lda vec_dst+1
-        pha
-
-        ; Relocate monitor.
-        lda #<monitor_src_end
-        sta vec_src
-        lda #>monitor_src_end
-        sta vec_src+1
-
-        lda #<monitor_dst_end
-        sta vec_dst
-        lda #>monitor_dst_end
-        sta vec_dst+1
-
-        ldy #0
+        ; Relocate monitor - copying back to front.
+        ;
+        ; Note:
+        ;   This code is known to always reside in RAM, so we can use
+        ;   self-modifying code.
 @copy_loop:
-        lda (vec_src),y
-        sta (vec_dst),y
+@rd:
+        lda monitor_src_end
+@wr:
+        sta monitor_dst_end
 
-        ; Decrement source vector.
-        lda vec_src
-        sec
-        sbc #1
-        sta vec_src
-        lda vec_src+1
-        sbc #0
-        sta vec_src+1
+        vec_dec @rd+1
+        vec_dec @wr+1
 
-        ; Decrement destination vector.
-        lda vec_dst
-        sec
-        sbc #1
-        sta vec_dst
-        lda vec_dst+1
-        sbc #0
-        sta vec_dst+1
-
-        ; Compare destination vector with monitor code start.
-        lda vec_src+1
-        cmp #>monitor_src_start
-        bne @copy_loop
-
-        lda vec_src
-        cmp #<monitor_src_start
+        vec_cmp_i16 @rd+1, monitor_src_start
         bcs @copy_loop
 
-        ; Restore zeropage pointers.
-        pla
-        sta vec_dst+1
-        pla
-        sta vec_dst
-        pla
-        sta vec_src+1
-        pla
-        sta vec_src
-
-        ; Enable interrupts again and jump to monitor entry point.
-        cli
+        ; Jump to monitor entry point.
         jmp __INIT_LOAD__
 
 .endif ; CONFIG_INIT_RELOCATE_MONITOR
