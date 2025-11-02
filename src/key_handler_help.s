008b04fb83e9263c770689e0b8b44c94150bf778
Wolfgang Reißnegger <w.reissnegger@gmx.net>
2025-11-01 18:07:42 -1000

    Fix help screen for large screens.

--- src/key_handler_help.s
+++ src/key_handler_help.s
@@ -85,50 +85,31 @@ handle_key_help_screen:
         print_str_at xpos, 1, "VMON (C) 1986-2025 W.REISSNEGGER"
         xpos .set xpos + 1
         xpos .set xpos + 1
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_SET_PROGRAM_COUNTER), " - SET PROGRAM COUNTER (ADDR)")
+        print_help                                                       KEY_SET_PROGRAM_COUNTER,       "SET PROGRAM COUNTER (ADDR)"
+        print_help                                                       KEY_SET_MEMORY_ADDRESS,        "SET MEMORY ADDRESS (ADDR)"
+        print_help_ifdef CONFIG_KEY_HANDLER_REG_SET,                     KEY_SET_REGISTER_VALUE,        "SET REGISTER VALUE (VAL)"
         xpos .set xpos + 1
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_SET_MEMORY_ADDRESS), " - SET MEMORY ADDRESS (ADDR)")
+        print_help                                                       KEY_SINGLE_STEP_INTO,          "SINGLE STEP COMMAND - STEP INTO"
+        print_help                                                       KEY_SINGLE_STEP_OVER,          "SINGLE STEP COMMAND - STEP OVER"
+        print_help_ifdef CONFIG_KEY_HANDLER_SINGLE_STEP_OVER_UNTIL_RTS,  KEY_CONT_STEP_OVER_UNTIL_RTS,  "CONTINUE EXECUTION (N) UNTIL RTS"
+        print_help_ifdef CONFIG_KEY_HANDLER_SINGLE_STEP_OVER_UNTIL_ADDR, KEY_CONT_STEP_OVER_UNTIL_ADDR, "CONTINUE EXECUTION (N) UNTIL (ADDR)"
+        print_help_ifdef CONFIG_KEY_HANDLER_SINGLE_STEP_INTO_UNTIL_RTS,  KEY_CONT_STEP_INTO_UNTIL_RTS,  "CONTINUE EXECUTION (S) UNTIL RTS"
+        print_help_ifdef CONFIG_KEY_HANDLER_RESUME_LAST_RUN_MODE,        KEY_RESME_LAST_RUN_MODE,       "CONTINUE LAST EXECUTION MODE"
         xpos .set xpos + 1
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_SET_REGISTER_VALUE), " - SET REGISTER VALUE (VAL)")
+        print_help                                                       KEY_FILL_MEMORY,               "FILL MEMORY (FROM-TO-VAL)"
+        print_help                                                       KEY_COPY_MEMORY,               "COPY MEMORY (FROM-TO-DEST)"
+        print_help                                                       KEY_EDIT_MEMORY,               "EDIT MEMORY (ADDR-VAL-...)"
         xpos .set xpos + 1
+        print_help_ifdef CONFIG_KEY_HANDLER_SCREEN_SHOW,                 KEY_SHOW_SCREEN,               "SHOW DEBUG SCREEN"
+        print_help_ifdef CONFIG_KEY_HANDLER_MEM_TOGGLE,                  KEY_TOGGLE_MEMORY_DISPLAY,     "TOGGLE MEMORY SIZE"
+        print_help                                                       KEY_MEMORY_SCROLL_UP,          "SCROLL UP MEMORY"
+        print_help                                                       KEY_MEMORY_SCROLL_DOWN,        "SCROLL DOWN MEMORY"
+        print_help                                                       KEY_MEMORY_PAGE_UP,            "PAGE UP MEMORY"
+        print_help                                                       KEY_MEMORY_PAGE_DOWN,          "PAGE DOWN MEMORY"
         xpos .set xpos + 1
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_SINGLE_STEP_INTO), " - SINGLE STEP COMMAND - STEP INTO")
+        print_str_at xpos, 0,                                                                           "CRSR UP/DN - SCROLL ASSEMBLY"
         xpos .set xpos + 1
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_SINGLE_STEP_OVER), " - SINGLE STEP COMMAND - STEP OVER")
-        xpos .set xpos + 1
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_CONT_STEP_OVER_UNTIL_RTS), " - CONTINUE EXECUTION (X) UNTIL RTS")
-        xpos .set xpos + 1
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_CONT_STEP_OVER_UNTIL_ADDR), " - CONTINUE EXECUTION (X) UNTIL (ADDR)")
-        xpos .set xpos + 1
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_CONT_STEP_INTO_UNTIL_RTS), " - CONTINUE EXECUTION (S) UNTIL RTS")
-        xpos .set xpos + 1
-        xpos .set xpos + 1
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_FILL_MEMORY), " - FILL MEMORY (FROM-TO-VAL)")
-        xpos .set xpos + 1
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_COPY_MEMORY), " - COPY MEMORY (FROM-TO-DEST)")
-        xpos .set xpos + 1
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_EDIT_MEMORY), " - EDIT MEMORY (ADDR-VAL-...)")
-        xpos .set xpos + 1
-        xpos .set xpos + 1
-.if CONFIG_KEY_HANDLER_SCREEN_SHOW
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_SHOW_SCREEN), " - SHOW DEBUG SCREEN")
-        xpos .set xpos + 1
-.endif ; CONFIG_KEY_HANDLER_SCREEN_SHOW
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_TOGGLE_MEMORY_DISPLAY), " - TOGGLE MEMORY SIZE")
-        xpos .set xpos + 1
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_MEMORY_SCROLL_UP), " - SCROLL UP MEMORY")
-        xpos .set xpos + 1
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_MEMORY_SCROLL_DOWN), " - SCROLL DOWN MEMORY")
-        xpos .set xpos + 1
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_MEMORY_PAGE_UP), " - PAGE UP MEMORY")
-        xpos .set xpos + 1
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_MEMORY_PAGE_DOWN), " - PAGE DOWN MEMORY")
-        xpos .set xpos + 1
-        xpos .set xpos + 1
-        print_str_at xpos, 0, "CRSR UP/DN - SCROLL ASSEMBLY"
-        xpos .set xpos + 1
-        xpos .set xpos + 1
-        print_str_at xpos, 0, .concat(.sprintf("%c", KEY_QUIT), " - QUIT")
+        print_help                                                       KEY_QUIT,                      "QUIT"
 .endif ; if screen_size_y < 40
 
         io_key_in_blocking
