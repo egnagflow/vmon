4d4a33b56a7963f6a70623f9f16e33cb827e4ba0
Wolfgang Reißnegger <w.reissnegger@gmx.net>
2025-11-02 11:19:59 -1000

    Version v1.1.0

--- src/version.h
+++ src/version.h
@@ -2,6 +2,6 @@
 ; NOTE:
 ;   Numbers are in BCD format.
 .define VERSION_MAJOR $01
-.define VERSION_MINOR $00
-.define VERSION_PATCH $01
+.define VERSION_MINOR $01
+.define VERSION_PATCH $00
 .endif
