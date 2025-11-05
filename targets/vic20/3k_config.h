a69191e2e3e7b6f674a3b4589175674de5a629a6
Wolfgang Reißnegger <w.reissnegger@gmx.net>
2025-11-05 10:37:59 -1000

    Be more generous with unexpanded and 3k VIC20 build options.
    Enable a few more options to make builds more useful.

--- targets/vic20/3k_config.h
+++ targets/vic20/3k_config.h
@@ -20,18 +20,29 @@ CONFIG_EXAMPLE_CODE                             := 1
 
 CONFIG_OPTIMIZE_MEM_SIZE                        := 1
 
-CONFIG_ENABLE_COLOREDIT_FONT                    := 1
 CONFIG_ENABLE_COLOR                             := 1
+CONFIG_ENABLE_COLOREDIT_FONT                    := 1
+CONFIG_ENABLE_COLOREDIT_BACKGROUND              := 1
+CONFIG_ENABLE_CURSOR_DISPLAY                    := 1
 
 CONFIG_PRG_HEADER                               := 1
 CONFIG_BASIC_SYS_LINE                           := 1
 CONFIG_BASIC_SYS_ADDR                           := $40D
 
+CONFIG_HANDLE_BRK                               := 1
 CONFIG_USE_BUILT_IN_KEYSCAN                     := 1
 CONFIG_STACK_RESTORE                            := 1
+
+CONFIG_KEY_HANDLER_SCREEN_SHOW                  := 1
 
 CONFIG_KEY_HANDLER_REG_SET                      := 1
 
 CONFIG_KEY_HANDLER_SINGLE_STEP_INTO_UNTIL_RTS   := 1
+CONFIG_KEY_HANDLER_SINGLE_STEP_OVER_UNTIL_RTS   := 1
+CONFIG_KEY_HANDLER_SINGLE_STEP_OVER_UNTIL_ADDR  := 1
+CONFIG_KEY_HANDLER_RESUME_LAST_RUN_MODE         := 1
 
 CONFIG_KEY_HANDLER_MEM_TOGGLE                   := 1
+CONFIG_KEY_HANDLER_MEM_EDIT                     := 1
+CONFIG_KEY_HANDLER_MEM_FILL                     := 1
+CONFIG_KEY_HANDLER_MEM_COPY                     := 1
